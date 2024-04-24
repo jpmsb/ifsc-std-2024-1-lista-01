077cad14ae566bc3f91a0f1f3cbf110916b8e962
João Pedro Menegali Salvan Bitencourt <joao.ms@aluno.ifsc.edu.br>
2024-04-24 15:50:27 -0300

    Aplicações de teste iniciais.

---
+++ cliente/cliente/app/src/main/java/engtelecom/std/Principal.java
@@ -0,0 +1,11 @@
+package engtelecom.std;
+
+public class Principal {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Principal().getGreeting());
+    }
+}
