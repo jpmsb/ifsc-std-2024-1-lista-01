c9f99b69722e3ad98452118facfdae6af8c4073a
João Pedro Menegali Salvan Bitencourt <joao.ms@aluno.ifsc.edu.br>
2024-04-27 17:15:11 -0300

    Reorganização do código que transferiu os métodos iniciaServidorMulticast, obterInterfacesDeRede e iniciaServidorTCP para uma classe separada. Além disso, o logger foi ajustado para utilizar somente o formatador personalizado criado. Ademais, ajustes estéticos.

--- servidor/servidor/app/src/main/java/engtelecom/std/Principal.java
+++ servidor/servidor/app/src/main/java/engtelecom/std/Principal.java
@@ -1,102 +1,41 @@
 package engtelecom.std;
 
-import java.net.ServerSocket;
-import java.net.Inet6Address;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.logging.ConsoleHandler;
 import java.util.logging.Logger;
-
-import engtelecom.std.multicast.ServidorMulticast;
-import engtelecom.std.tcp.ThreadServidor;
+import java.util.logging.LogManager;
 
 public class Principal {
-    // Variável para armazenar o endereço IP do servidor
-    private static String proprioEnderecoIp;
+    // Porta padrão para o servidor de descoberta
+    private static final int PORTA_MULTICAST_PADRAO = 8888;
+    
+    // Porta padrão para o servidor TCP
+    private static final int PORTA_TCP_PADRAO = 51000;
 
-    // Para exibir mensagens de log
-    private static Logger logger = Logger.getLogger(Principal.class.getName());
-
-    // Variável para controlar a execução da thread principal
-    // volatile garante que a variável running será sempre lida diretamente da memória principal e não de uma cópia em cache do processador
-    private static volatile boolean running = true;
-
-    public void iniciaServidorMulticast(int porta, String enderecoMulticast, int portaTcp){
-        try {
-            ServidorMulticast servidorMulticast = new ServidorMulticast(enderecoMulticast, porta, portaTcp);
-
-            // Inicia a thread do tcp de hora multicast
-            new Thread(servidorMulticast).start();
-
-        } catch (UnknownHostException | SocketException e) {
-            logger.severe("Erro: " + e.getMessage());
-        }
-    }
-
-    public void iniciaServidorTCP(int porta){
-        // Adiciona um tratador para encerrar o processo quando o usuário pressionar CTRL+C
-        Runtime.getRuntime().addShutdownHook( new Thread(() -> running = false));
-
-        System.out.println("\u2591\u2592\u2592 Iniciando o tcp \u2592\u2592\u2591");
-        
-        try (ServerSocket serverSocket = new ServerSocket(porta)) {
-            
-            System.out.println("Servidor aguardando conexões em " + serverSocket.getInetAddress() +":" + porta);
-            System.out.println("Pressione CTRL+C para encerrar o processo do tcp\n\n");
-
-            while (running) {
-                // Aceita a conexão de um cliente e cria uma nova thread para atendê-lo
-                new Thread(new ThreadServidor(serverSocket.accept())).start();
-            }
-
-        } catch (Exception e) {
-            logger.severe("Erro: " + e.getMessage());
-        }
-    }
-
-    public List<InetAddress> obterInterfacesDeRede() {
-        List<InetAddress> interfaces = new ArrayList<>();
-        try {
-            logger.info("Obtendo interfaces de rede...");
-            for (NetworkInterface networkInterface : Collections.list(NetworkInterface.getNetworkInterfaces())) {
-                for (InetAddress inetAddress : Collections.list(networkInterface.getInetAddresses())) {
-                    // Não incluir loopback e IPv6ß
-                    if (inetAddress.isLoopbackAddress() || inetAddress instanceof Inet6Address) {
-                        continue;
-                    }
-                    interfaces.add(inetAddress);
-                    String m = String.format("Nome: %s\tEndereço: %s\n", networkInterface.getDisplayName(), inetAddress.toString().substring(1));
-                    logger.info(m);
-                }
-            }
-        } catch (Exception e) {
-            logger.severe("Erro: " + e.getMessage());
-        }
-        return interfaces;
-    }
+    // Logger geral para exibir mensagens de log
+    private static Logger logger = Logger.getLogger(Servidor.class.getName());
+    private static ConsoleHandler consoleHandler = new ConsoleHandler();
 
     public static void main(String[] args) {
-        int porta = 8888;
+        LogManager.getLogManager().reset();
+
+        consoleHandler.setFormatter(new FormatadorDeLog());
+        logger.addHandler(consoleHandler);
+
+        int portaMulticast = PORTA_MULTICAST_PADRAO;
         String enderecoMulticast = "231.0.0.0";
-        int portaTcp = 51000;
+        int portaServidorTcp = PORTA_TCP_PADRAO;
 
         // Caso algum argumento seja informado, obtém as informações
         // de endereço multicast e porta
-        if (args.length == 2) {
-            enderecoMulticast = args[0];
-            porta = Integer.parseInt(args[1]);
+        if (args.length == 4) {
+            enderecoMulticast = args[1];
+            portaMulticast = Integer.parseInt(args[2]);
+            portaServidorTcp = Integer.parseInt(args[3]);
         }
 
-        Principal principal = new Principal();
-        List<InetAddress> enderecos =  principal.obterInterfacesDeRede();
-        proprioEnderecoIp = enderecos.get(0).toString().substring(1);
-
-        principal.iniciaServidorMulticast(porta, enderecoMulticast, portaTcp);
-
-        principal.iniciaServidorTCP(portaTcp);
+        Servidor servidor = new Servidor(logger);
+        servidor.obterInterfacesDeRede();
+        servidor.iniciaParteMulticast(portaMulticast, enderecoMulticast, portaServidorTcp);
+        servidor.iniciaParteTcp(portaServidorTcp);
     }
 }
